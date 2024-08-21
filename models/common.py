361cad82db9760bbb862b3652525f68a75846bf9
Aquino Nyapara <aquino.nyapara@gmail.com>
2024-08-21 11:19:49 +0300

    Update common.py

--- models/common.py
+++ models/common.py
@@ -25,7 +25,32 @@ def autopad(k, p=None):  # kernel, padding
     if p is None:
         p = k // 2 if isinstance(k, int) else [x // 2 for x in k]  # auto-pad
     return p
+class SimAM(torch.nn.Module):
+    def __init__(self, channels = None,out_channels = None, e_lambda = 1e-4):
+        super(SimAM, self).__init__()
 
+        self.activaton = nn.Sigmoid()
+        self.e_lambda = e_lambda
+
+    def __repr__(self):
+        s = self.__class__.__name__ + '('
+        s += ('lambda=%f)' % self.e_lambda)
+        return s
+
+    @staticmethod
+    def get_module_name():
+        return "simam"
+
+    def forward(self, x):
+
+        b, c, h, w = x.size()
+        
+        n = w * h - 1
+
+        x_minus_mu_square = (x - x.mean(dim=[2,3], keepdim=True)).pow(2)
+        y = x_minus_mu_square / (4 * (x_minus_mu_square.sum(dim=[2,3], keepdim=True) / n + self.e_lambda)) + 0.5
+
+        return x * self.activaton(y)  
 
 class MP(nn.Module):
     def __init__(self, k=2):
