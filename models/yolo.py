6f20cabd29db24c7acbf9ecae4f5861fa4318749
Aquino Nyapara <aquino.nyapara@gmail.com>
2024-08-21 11:21:26 +0300

    Update yolo.py

--- models/yolo.py
+++ models/yolo.py
@@ -797,6 +797,12 @@ def parse_model(d, ch):  # model_dict, input_channels(3)
             c2 = ch[f] * args[0] ** 2
         elif m is Expand:
             c2 = ch[f] // args[0] ** 2
+        elif m is SimAM:
+            c1, c2 = ch[f], args[0]
+            if c2 != no:
+                c2 = make_divisible(c2 * gw, 8)
+            args = [c1, c2]
+            
         else:
             c2 = ch[f]
 
